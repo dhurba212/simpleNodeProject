1bc683e0e7896d74e08dd583405aefa4fb653c16
dhurba <dhurbag21@gmail.com>
2021-04-18 20:28:23 -0400

    created simple node project with express

---
+++ notes.js
@@ -0,0 +1,10 @@
+// to begin inside a new folder //npm init//it creates a package.json
+// inside package.json in scripts //"start": "node index.js"//this let us run the appliction
+//just by doing npm start, otherwise we can just do (node index.js)
+
+//node uses Express as a backend server, so need to install it
+    //npm install express
+    //inside index.js now we can create routes by using express
+    //In java we import the dependencies, but here in node, we do: const express = require("express")//see index.js
+    //any installed package u want to use, u need to do const name = require("package name");
+    
